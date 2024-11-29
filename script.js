d84e57e5bf8a9cac805ff04ec76f8ad67a85acf0
Seidii <115049800+Seidii@users.noreply.github.com>
2024-11-29 10:11:55 -0300

    Update script.js
    ajuste

--- script.js
+++ script.js
@@ -17,50 +17,49 @@ document.addEventListener('DOMContentLoaded', () => {
         const selectedSection = document.getElementById(sectionId);
         if (selectedSection) {
             selectedSection.classList.add('active');
-            selectedSection.style.display = 'block'; // Exibe a seção
+            selectedSection.style.display = 'block'; 
         }
     
         if (sectionId === 'home') {
-            summarySection.classList.add('active'); // Exibe a seção summary
-            summarySection.style.display = 'block'; // Exibe o summary
+            summarySection.classList.add('active'); 
+            summarySection.style.display = 'block'; 
         } else {
-            summarySection.classList.remove('active'); // Oculta a seção summary
-            summarySection.style.display = 'none'; // Oculta o summary
+            summarySection.classList.remove('active'); 
+            summarySection.style.display = 'none'; 
         }
     }
     
     function showSection(sectionId) {
-        // Oculta todas as seções
+        
         sections.forEach(section => {
-            section.classList.remove('active'); // Remove a classe 'active'
-            section.style.display = 'none'; // Oculta a seção
+            section.classList.remove('active'); 
+            section.style.display = 'none'; 
         });
     
         // Exibe a seção correspondente
         const selectedSection = document.getElementById(sectionId);
         if (selectedSection) {
             selectedSection.classList.add('active');
-            selectedSection.style.display = 'block'; // Exibe a seção
+            selectedSection.style.display = 'block';
         }
     
-        // Exibe ou oculta a seção summary
         if (sectionId === 'home') {
-            summarySection.classList.add('active'); // Exibe a seção summary
-            summarySection.style.display = 'block'; // Exibe o summary
+            summarySection.classList.add('active'); 
+            summarySection.style.display = 'block'; 
         } else {
-            summarySection.classList.remove('active'); // Oculta a seção summary
-            summarySection.style.display = 'none'; // Oculta o summary
+            summarySection.classList.remove('active'); 
+            summarySection.style.display = 'none';
         }
     
-        // Exibe ou oculta a seção valores
+        
         const valoresSection = document.querySelector('.valores');
         if (valoresSection) {
             if (sectionId === 'quem-somos') {
-                valoresSection.classList.add('active'); // Exibe a seção valores
-                valoresSection.style.display = 'block'; // Exibe a seção
+                valoresSection.classList.add('active'); 
+                valoresSection.style.display = 'block';
             } else {
-                valoresSection.classList.remove('active'); // Oculta a seção valores
-                valoresSection.style.display = 'none'; // Oculta a seção
+                valoresSection.classList.remove('active'); 
+                valoresSection.style.display = 'none'; 
             }
         }
     }
@@ -70,23 +69,23 @@ document.addEventListener('DOMContentLoaded', () => {
     // Adiciona evento de clique aos links do menu
     links.forEach(link => {
         link.addEventListener('click', (event) => {
-            event.preventDefault(); // Evita o comportamento padrão do link
-            const sectionId = link.getAttribute('data-section'); // Obtém o ID da seção a ser exibida
-            showSection(sectionId); // Chama a função para exibir a seção
+            event.preventDefault(); 
+            const sectionId = link.getAttribute('data-section'); 
+            showSection(sectionId); 
         });
     });
 });
 
 
 document.addEventListener('DOMContentLoaded', () => {
-    const links = document.querySelectorAll('nav a'); // Seleciona todos os links do menu
-    const sections = document.querySelectorAll('.section'); // Seleciona todas as seções
-    const summarySection = document.querySelector('.summary'); // Seleciona a seção summary
+    const links = document.querySelectorAll('nav a'); 
+    const sections = document.querySelectorAll('.section'); 
+    const summarySection = document.querySelector('.summary'); 
 
     function showSection(sectionId) {
         // Oculta todas as seções
         sections.forEach(section => {
-            section.classList.remove('active'); // Remove a classe 'active'
+            section.classList.remove('active'); 
             section.style.display = 'none'; // Oculta a seção
         });
     
@@ -94,62 +93,62 @@ document.addEventListener('DOMContentLoaded', () => {
         const selectedSection = document.getElementById(sectionId);
         if (selectedSection) {
             selectedSection.classList.add('active');
-            selectedSection.style.display = 'block'; // Exibe a seção
+            selectedSection.style.display = 'block'; 
         }
     
         if (sectionId === 'home') {
-            summarySection.classList.add('active'); // Exibe a seção summary
-            summarySection.style.display = 'block'; // Exibe o summary
+            summarySection.classList.add('active'); 
+            summarySection.style.display = 'block'; 
         } else {
-            summarySection.classList.remove('active'); // Oculta a seção summary
-            summarySection.style.display = 'none'; // Oculta o summary
+            summarySection.classList.remove('active'); 
+            summarySection.style.display = 'none'; 
         }
     }
     
     function showSection(sectionId) {
         // Oculta todas as seções
         sections.forEach(section => {
-            section.classList.remove('active'); // Remove a classe 'active'
-            section.style.display = 'none'; // Oculta a seção
+            section.classList.remove('active'); 
+            section.style.display = 'none'; 
         });
     
         // Exibe a seção correspondente
         const selectedSection = document.getElementById(sectionId);
         if (selectedSection) {
             selectedSection.classList.add('active');
-            selectedSection.style.display = 'block'; // Exibe a seção
+            selectedSection.style.display = 'block'; 
         }
     
         // Exibe ou oculta a seção summary
         if (sectionId === 'home') {
-            summarySection.classList.add('active'); // Exibe a seção summary
-            summarySection.style.display = 'block'; // Exibe o summary
+            summarySection.classList.add('active'); 
+            summarySection.style.display = 'block'; 
         } else {
-            summarySection.classList.remove('active'); // Oculta a seção summary
-            summarySection.style.display = 'none'; // Oculta o summary
+            summarySection.classList.remove('active'); 
+            summarySection.style.display = 'none'; 
         }
     
         // Exibe ou oculta a seção valores
         const valoresSection = document.querySelector('.valores');
         if (valoresSection) {
             if (sectionId === 'quem-somos') {
-                valoresSection.classList.add('active'); // Exibe a seção valores
-                valoresSection.style.display = 'block'; // Exibe a seção
+                valoresSection.classList.add('active'); 
+                valoresSection.style.display = 'block';
             } else {
-                valoresSection.classList.remove('active'); // Oculta a seção valores
-                valoresSection.style.display = 'none'; // Oculta a seção
+                valoresSection.classList.remove('active'); 
+                valoresSection.style.display = 'none'; o
             }
         }
     }
     
     
 
-    // Adiciona evento de clique aos links do menu
+    
     links.forEach(link => {
         link.addEventListener('click', (event) => {
-            event.preventDefault(); // Evita o comportamento padrão do link
-            const sectionId = link.getAttribute('data-section'); // Obtém o ID da seção a ser exibida
-            showSection(sectionId); // Chama a função para exibir a seção
+            event.preventDefault(); 
+            const sectionId = link.getAttribute('data-section'); 
+            showSection(sectionId); 
         });
     });
 });
